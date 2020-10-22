fb3e775e02598cc3d48d2d2dccd4ada3be930aab
Neil <neil.mccoy@outlook.com>
2020-10-22 13:05:38 +1100

    New basic boiler plate code

---
+++ Redirxn.TeamKitty/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Redirxn.TeamKitty.ViewModels;
+using Redirxn.TeamKitty.Views;
+using Xamarin.Forms;
+
+namespace Redirxn.TeamKitty
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Redirxn.TeamKitty/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Redirxn.TeamKitty.ViewModels;
+
+namespace Redirxn.TeamKitty.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
